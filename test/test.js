05eaddff74059528c070d9fb23f8e36ac93439c3
Ricardo Almeida <ricardo94almeida@gmail.com>
2018-11-02 16:49:41 +0000

    Finish working and refactored palindrome method

---
+++ test/test.js
@@ -0,0 +1,35 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function() {
+
+  describe("#palindrome", function() {
+
+    it("should return false for a non-palindrome", function() {
+      let nonPalindrome = new Phrase("apple");
+      assert(!nonPalindrome.palindrome());
+    });
+
+    it("should return true for a plain palindrome", function() {
+      let plainPalindrome = new Phrase("racecar");
+      assert(plainPalindrome.palindrome());
+    });
+
+    it("should return true for a mixed-case palindrome", function() {
+      let casePalindrome = new Phrase("RaceCar");
+      assert(casePalindrome.palindrome());
+    });
+
+    it("should return true for a palindrome with punctuation", function() {
+      let punctPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctPalindrome.palindrome());
+    })
+  });
+
+  describe("#letter", function() {
+    it("should return only letters", function() {
+      let punctPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual(punctPalindrome.letters(), "MadamImAdam");
+    });
+  });
+});
